d91175bcab66a5c41586980d457d5faada077e26
Carson <cpsievert1@gmail.com>
2022-11-17 10:39:22 -0600

    Add test

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,5 @@
 #' @import htmltools
-#' @import rlang missing_arg is_missing
+#' @importFrom rlang missing_arg is_missing
 #' @importFrom reactR createReactShinyInput
 #' @importFrom graphics hist
 #' @importFrom utils modifyList
